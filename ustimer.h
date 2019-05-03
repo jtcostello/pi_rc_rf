4d5cf9a77d3f73402997483261261fafcba1c020
Joey Costello <joeyc.swim@gmail.com>
2019-05-02 20:27:01 -0400

    ustimer h file

---
+++ ustimer.h
@@ -0,0 +1,12 @@
+// from https://yosh.ke.mu/article/raspberry_pi_getting_time
+
+#ifndef _USTIMER_H_
+#define _USTIMER_H_
+
+#define BCM2708_ST_BASE 0x20003000 /* BCM 2835 System Timer */
+
+unsigned int getUSTime();
+
+void initUScounter();
+
+#endif
